92c51495a3a78bc112ebe5c55e1b6fa36922cd9f
SalaccI4 <66245600+SalaccI4@users.noreply.github.com>
2025-09-27 17:39:00 -0700

    Finish implementing all CSS for calculator

---
+++ javascript.js
@@ -0,0 +1,28 @@
+let num1 = 2
+let num2 = 5
+let op
+
+function add(a, b){
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function operate(n1, n2, operator){
+
+}
+
+console.log(add(num1, num2))
+console.log(subtract(num1, num2))
+console.log(multiply(num1, num2))
+console.log(divide(num1, num2))
